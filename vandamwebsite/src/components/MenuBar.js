034254d78cfbb899fcfd399ce80da7c0b81d45f3
sabatermiguelalfonso <sabatermiguel11@gmail.com>
2023-05-15 19:44:25 +0800

    Feat: Added elements on the Menubar and installed lottie react

--- vandamwebsite/src/components/MenuBar.js
+++ vandamwebsite/src/components/MenuBar.js
@@ -1,10 +1,19 @@
 import React from "react";
+import "./MenuBar.css"
 
 const MenuBar = () => {
 
 return(
     <div>
+        <div className="ContainerMenuBar"> 
+                <div className="logoContainer">
+                <h1 >MIGUEL SABATER</h1>
+                </div>
 
+                <div className="menubarContainer">
+
+                </div>
+        </div>
 
 
         
